4ca23fdca2b308f51a4f0f4599634777df8cebc5
Bogdan Klimchuk <bogd.moen@yandex.com>
2021-03-07 17:00:28 +0300

    negative test implemented

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -1,32 +1,62 @@
 package ru.netology.web;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
 
 import static com.codeborne.selenide.Selenide.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class CallbackTest {
 
     @Test
-    void shouldTestV1() {
+    void shouldGetSuccessForm() {
         open("http://localhost:9999");
         $("[class='input__control'][type='text']").setValue("Василий");
         $("[class='input__control'][type='tel']").setValue("+79270000000");
         $(".checkbox__box").click();
         $("[type='button'][class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
         $(".Success_successBlock__2L3Cw").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldGetEmptyNameAlert() {
+        open("http://localhost:9999");
+        $("[type='button'][class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $("span.input_invalid > span > span.input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldGetEmptyPhoneAlert() {
+        open("http://localhost:9999");
+        $("[class='input__control'][type='text']").setValue("Василий");
+        $("[type='button'][class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $("[data-test-id='phone'].input_invalid > span > span.input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldGetIncorrectNameAlert() {
+        open("http://localhost:9999");
+        $("[class='input__control'][type='text']").setValue("test");
+        $("[class='input__control'][type='tel']").setValue("+79270000000");
+        $("[type='button'][class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $("[data-test-id='name'].input_invalid > span > span.input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldGetIncorrectPhoneAlert() {
+        open("http://localhost:9999");
+        $("[class='input__control'][type='text']").setValue("Иван");
+        $("[class='input__control'][type='tel']").setValue("955");
+        $("[type='button'][class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $("[data-test-id='phone'].input_invalid > span > span.input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldGetCheckboxAlert() {
+        open("http://localhost:9999");
+        $("[class='input__control'][type='text']").setValue("Иван");
+        $("[class='input__control'][type='tel']").setValue("+79270000000");
+        $("[type='button'][class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $(".input_invalid[data-test-id='agreement']").shouldHave(Condition.exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
 
 }
