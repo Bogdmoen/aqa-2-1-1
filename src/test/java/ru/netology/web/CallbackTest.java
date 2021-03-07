43a906133862ae23a5241a7facde176e4227ba53
Bogdan Klimchuk <bogd.moen@yandex.com>
2021-03-07 07:03:55 +0300

    selenide configured

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -1,5 +1,7 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -12,40 +14,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 
+import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CallbackTest {
-    private WebDriver driver;
-
-    @BeforeAll
-    static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-
-
-    }
-
-    @BeforeEach
-    void setUp() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless");
-        driver = new ChromeDriver(chromeOptions);
-    }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
-    }
 
     @Test
     void shouldTestV1() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[class='input__control'][type='text']")).sendKeys("Василий");
-        driver.findElement(By.cssSelector("[class='input__control'][type='tel']")).sendKeys("+79270000000");
-        driver.findElement(By.cssSelector(".checkbox__box")).click();
-        driver.findElement(By.cssSelector("[type='button'][class='button button_view_extra button_size_m button_theme_alfa-on-white']")).click();
-        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+        open("http://localhost:9999");
+        $("[class='input__control'][type='text']").setValue("Василий");
+        $("[class='input__control'][type='tel']").setValue("+79270000000");
+        $(".checkbox__box").click();
+        $("[type='button'][class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $(".Success_successBlock__2L3Cw").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
 }
